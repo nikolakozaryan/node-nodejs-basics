17d346d9ffa881c569eb018144019133753bafda
Nikolay <kolyak770@gmail.com>
2024-10-06 21:07:45 +0300

    add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const res = [];
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].startsWith("--")) {
+      res.push(`${argv[i].replace("--", "")} is ${argv[i + 1]}`);
+      i++;
+    }
+  }
+
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+import { env } from "process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const res = Object.entries(env)
+    .reduce((acc, [key, value]) => {
+      if (key.startsWith("RSS_")) {
+        acc.push(`${key}=${value}`);
+      }
+
+      return acc;
+    }, [])
+    .join("; ");
+
+  console.log(res);
 };
 
 parseEnv();
