8504e107d1490558422d774bab450a658ed2ee1f
Nikolay <kolyak770@gmail.com>
2024-10-07 19:34:52 +0300

    add child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from "node:child_process";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { stdin, stdout } from "node:process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const PATH_TO_FILE = resolve(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn("node", [PATH_TO_FILE, ...args]);
+
+  childProcess.stdout.pipe(stdout);
+
+  stdin.pipe(childProcess.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5]);
