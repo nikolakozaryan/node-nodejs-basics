1d4e7d733c7125e641d2195d9bddb086c085ae61
Nikolay <kolyak770@gmail.com>
2024-10-07 18:38:51 +0300

    add streams

---
+++ src/streams/constants.js
@@ -0,0 +1,5 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+export const FILES_DIRECTORY = "files";
+export const __dirname = dirname(fileURLToPath(import.meta.url));
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "node:fs";
+import { resolve } from "node:path";
+import { stdin } from "process";
+import { __dirname, FILES_DIRECTORY } from "./constants.js";
+
+const FILE_NAME = "fileToWrite.txt";
+
+const PATH_TO_FILE = resolve(__dirname, FILES_DIRECTORY, FILE_NAME);
+
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(PATH_TO_FILE, { flags: "a" });
+
+  stdin.pipe(writeStream);
 };
 
 await write();
